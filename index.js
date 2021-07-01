56384f9a497277a2197c726c0591616fd883bb2d
tokhanh <kto2303@gmail.com>
2021-07-01 18:26:07 +0700

    test button

---
+++ index.js
@@ -0,0 +1,3 @@
+import Button from './src/components/button'
+
+module.exports = { Button }
---
+++ src/components/button/Button.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let onClick
+    export let content
+</script>
+
+<style>
+    button {
+        background-color: #50d9ff;
+        border-radius: 5px;
+        border: transparent;
+    }
+
+    button:active {
+        background-color: #28c2ec;
+    }
+</style>
+
+<button on:click={_ => onClick()}>
+    {content || 'Button'}
+</button>
