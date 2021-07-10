273e5ff8a7f3e6c1d0c4f6162363ffdbb0bb9deb
tokhanh <kto2303@gmail.com>
2021-07-10 23:18:21 +0700

    init package

---
+++ example/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { Pricing, Card } from '@presscommandz/svelte-component'
+</script>
+
+<main>
+	<Pricing title="Find the right plan" subtitle="Join the revolution" note="Flexible pricing options for freelancers and design teams.">
+		<Card price="9$/ months"/>
+		<Card price="9$/ month"/>
+		<Card price="9$/ month"/>
+	</Pricing>
+</main>
+
+
---
+++ index.js
@@ -0,0 +1,4 @@
+import Pricing from './src/promotion/Pricing.svelte'
+import Card from './src/promotion/Card.svelte'
+
+export { Pricing, Card }
