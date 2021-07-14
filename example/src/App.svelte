3b51789e9dbc17a69406ff41fbd327bef53eceac
tokhanh <kto2303@gmail.com>
2021-07-14 23:42:29 +0700

    node_modules

--- example/src/App.svelte
+++ example/src/App.svelte
@@ -1,10 +1,10 @@
 <script>
-	import { Pricing, Card } from '@presscommandz/svelte-component'
+	import { Pricing, Card } from '../../index'
 </script>
 
 <main>
 	<Pricing title="Find the right plan" subtitle="Join the revolution" note="Flexible pricing options for freelancers and design teams.">
-		<Card price="9$/ months"/>
+		<Card price="9$/ months" />
 		<Card price="9$/ month"/>
 		<Card price="9$/ month"/>
 	</Pricing>
