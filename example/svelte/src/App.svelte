673aad09261f25e5579bb514bd25d06ca8c4e0e9
tokhanh <kto2303@gmail.com>
2021-07-31 14:03:47 +0700

    add example

--- example/svelte/src/App.svelte
+++ example/svelte/src/App.svelte
@@ -1,60 +1,74 @@
 <script>
     import { Promotion } from "@presscommandz/svelte-component"
-    import { PromotionViewModel } from "@presscommandz/svelte-component"
-    let data = {
-        subtitle: "Hello World",
-        listCard: [
-            {
-                title: "Free",
-                button: "Get free",
-                drawbacks: [
-                    "Limitted diagram",
-                    "Change theme diagram",
-                    "50 nodes per diagram",
-                    "Use custom color",
-                    "2 contributors",
-                    "Save in cloud",
-                    "Limitted export"
-                ],
-                advances: ["Default color set"]
-            },
-            {
-                title: "Pro",
-                button: "Get pro",
-                price: 99,
-                drawbacks: ["Limitted diagram", "Limitted change theme"],
-                advances: [
-                    "1000 nodes per diagram",
-                    "Use custom color",
-                    "5 contributors",
-                    "Save in cloud",
-                    "Full export options",
-                    "Default color set"
-                ]
-            },
-            {
-                title: "Premium",
-                button: "Get premium",
-                price: 199,
-                advances: [
-                    "Unlimitted diagram",
-                    "Unlimmited change theme",
-                    "Unlimitted node",
-                    "Use custom color",
-                    "Unlimitted contributors",
-                    "Save in cloud",
-                    "Full export options",
-                    "Default color set"
-                ]
-            }
-        ]
-    }
 
-    const promotionViewModel = new PromotionViewModel(data)
+    const {
+        PromotionView,
+        PricingPlanModel,
+        PromotionModel,
+        PromotionViewModel
+    } = Promotion
+
+    let card_1 = new PricingPlanModel({
+        planTitle: "Free",
+        planPrice: 0,
+        textButton: "Get free",
+        drawbacks: [
+            "Limitted diagram",
+            "Change theme diagram",
+            "50 nodes per diagram",
+            "Use custom color",
+            "2 contributors",
+            "Save in cloud",
+            "Limitted export"
+        ],
+        advances: ["Default color set"]
+    })
+
+    let card_2 = new PricingPlanModel({
+        planTitle: "Pro",
+        textButton: "Get pro",
+        planPrice: 99,
+        unit: "$",
+        drawbacks: ["Limitted diagram", "Limitted change theme"],
+        advances: [
+            "1000 nodes per diagram",
+            "Use custom color",
+            "5 contributors",
+            "Save in cloud",
+            "Full export options",
+            "Default color set"
+        ]
+    })
+    let card_3 = new PricingPlanModel({
+        planTitle: "Premium",
+        textButton: "Get premium",
+        planPrice: 199,
+        unit: "$",
+        advances: [
+            "Unlimitted diagram",
+            "Unlimmited change theme",
+            "Unlimitted node",
+            "Use custom color",
+            "Unlimitted contributors",
+            "Save in cloud",
+            "Full export options",
+            "Default color set"
+        ]
+    })
+
+    let data = new PromotionModel({
+        listPricingPlanCard: [card_1, card_2, card_3]
+    })
+    let promotionViewModel = new PromotionViewModel(data)
+
+    function getAlert(e) {
+        alert(e.detail.planTitle)
+    }
 </script>
 
-<Promotion
+<PromotionView
     viewModel={promotionViewModel}
     backgroundColor={"red"}
     heightOfCard={390}
+    on:getPlanInformation={getAlert}
 />
